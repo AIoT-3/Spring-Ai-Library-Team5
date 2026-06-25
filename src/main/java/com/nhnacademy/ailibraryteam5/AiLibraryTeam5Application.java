c2790069101291e11476c85790f58cf624ed1298
Wjdwodud2525 <nury252525353535@gmail.com>
2026-06-25 14:04:16 +0900

    init

---
+++ src/main/java/com/nhnacademy/ailibraryteam5/AiLibraryTeam5Application.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.ailibraryteam5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLibraryTeam5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiLibraryTeam5Application.class, args);
+    }
+
+}
