1f7b6000649e4d2d58e9e6003cb3981bc71a566f
Wjdwodud2525 <nury252525353535@gmail.com>
2026-06-29 17:37:20 +0900

    검색 부분 완료 80%

---
+++ src/main/java/com/nhnacademy/ailibraryteam5/core/book/rag/cache/RagProperties.java
@@ -0,0 +1,26 @@
+package com.nhnacademy.ailibraryteam5.core.book.rag.cache;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "app.rag")
+public class RagProperties {
+
+    private int candidates = 5;
+    private SemanticCache semanticCache = new SemanticCache();
+
+    @Getter
+    @Setter
+    public static class SemanticCache{
+        private boolean enabled = true;
+        private long ttlMinutes = 100;
+        private double similarityThreshold = 0.8;
+        private int maxCandidatesToCompare = 5;
+        private int maxIndexSize = 2000;
+        private String version = "rag-semantic:v1";
+    }
+}
