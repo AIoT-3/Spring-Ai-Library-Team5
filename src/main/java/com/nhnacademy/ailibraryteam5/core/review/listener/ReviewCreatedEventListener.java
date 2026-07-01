13fb13762b9e4d93acbb0fd8f73ef1f2d4ec216a
Duwing <ssuk0406@gmail.com>
2026-07-01 09:34:19 +0900

    리뷰 요약 생성 이벤트 발생시 버벅임 문제 개선

--- src/main/java/com/nhnacademy/ailibraryteam5/core/review/listener/ReviewCreatedEventListener.java
+++ src/main/java/com/nhnacademy/ailibraryteam5/core/review/listener/ReviewCreatedEventListener.java
@@ -4,6 +4,7 @@ import com.nhnacademy.ailibraryteam5.core.review.event.ReviewCreatedEvent;
 import com.nhnacademy.ailibraryteam5.core.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -14,6 +15,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class ReviewCreatedEventListener {
     private final ReviewService reviewService;
 
+    @Async("taskExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleReviewCreatedEvent(ReviewCreatedEvent event){
         log.info("리뷰 생성 이벤트 수신 : booId={}", event.bookId());
